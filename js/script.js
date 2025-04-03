7dabe964f2567cb64944823229fa5a0e79c59ce0
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-03 15:03:52 +0200

    [UPD] add js dom elements and variables

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// recupero gli elementi del DOM che mi servono
+
+const mailList = document.getElementById('lista-mail');
+
+// dichiaro le variabili che mi servono
+
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`;
+
