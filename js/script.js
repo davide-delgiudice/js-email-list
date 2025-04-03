83fce0036ac2d19737fb961492fafad115d503c2
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-03 17:36:22 +0200

    [UPD] removed array and add li with mails in DOM - Esercizio Terminato

--- js/script.js
+++ js/script.js
@@ -6,17 +6,15 @@ const mailList = document.getElementById('lista-mail');
 
 const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`;
 
-const allMail = [];
-
 function getEmail() {
-
     for(i=0; i<10; i++) {
         axios.get(endpoint).then( mail => {
             let oneMail = mail.data.response;
             if(oneMail[i] != oneMail){
-                allMail.push(oneMail);
+                const listItem = document.createElement('li');
+                listItem.innerHTML = oneMail;
+                mailList.appendChild(listItem);
             }
-            console.log(allMail);
         })
     }
 };
